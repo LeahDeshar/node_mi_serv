17aba965e1eb3ce93cd6f66c133b51c69ce6dbea
Leah Deshar <leah.desar@gmail.com>
2024-07-12 10:46:58 +0545

    post: middleware

---
+++ post/controller/Postcontroller.js
@@ -0,0 +1,29 @@
+class PostController{
+    static async index(req,res){
+        try {
+            const posts = await prisma.post.findMany({});
+            return res.status(200).json(posts);
+        } catch (error) {
+            return res.status(500).json({message: error.message});
+        }
+    }
+
+    static async store(res,res){
+       try {
+         const authUser = req.user;
+         const {title, content} = req.body;
+ 
+         const post = await prisma.post.create({
+             data:{
+                 title,
+                 content,
+             }
+         });
+ 
+         return res.status(201).json(post);
+       } catch (error) {
+        return res.status(500).json({message: error.message});
+       }
+
+    }
+}
